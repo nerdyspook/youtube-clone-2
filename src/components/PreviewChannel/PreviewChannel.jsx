fe66b4cf4ff8404cc25522f0ad572bae85e1f5ce
Susanto <nerdyspook007@gmail.com>
2022-04-25 15:21:51 +0530

    added channel page

--- src/components/PreviewChannel/PreviewChannel.jsx
+++ src/components/PreviewChannel/PreviewChannel.jsx
@@ -1,8 +1,75 @@
 import React from "react";
+import img from "../../assets/channelArt.jpg";
+import { Avatar, Button } from "@material-ui/core";
 import "./PreviewChannel.scss";
+import VideoSmall from "../VideoSmall/VideoSmall";
 
 const PreviewChannel = () => {
-    return <div>PreviewChannel</div>;
+    return (
+        <div className="channel">
+            <img src={img} alt="" className="channel__art" />
+
+            <div className="channel__details">
+                <div className="channel__detailsWrap">
+                    <div className="channel__avatarWrap">
+                        <Avatar className="channel__avatar" />
+
+                        <div className="videothumb__channel">
+                            <h1 className="videothumb__title channel__title">
+                                NerdySpook
+                            </h1>
+
+                            <p className="videothumb__text watch__subCount">
+                                1M subscribers
+                            </p>
+                        </div>
+                    </div>
+
+                    <Button
+                        className="watch__subBtn channel__subBtn"
+                        color="primary"
+                        variant="contained"
+                    >
+                        Subscribe
+                    </Button>
+                </div>
+
+                <div className="channel__links">
+                    <div className="channel__link">
+                        <p>Home</p>
+                    </div>
+
+                    <div className="channel__link channel__link--active">
+                        <p>Videos</p>
+                        <div className="channel__link__border"></div>
+                    </div>
+
+                    <div className="channel__link">
+                        <p>About</p>
+                    </div>
+
+                    <div className="channel__link">
+                        <p>Playlist</p>
+                    </div>
+
+                    <div className="channel__link">
+                        <p>About</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="channel__content">
+                <div className="channel__contentWrapper">
+                    <VideoSmall channelView />
+                    <VideoSmall channelView />
+                    <VideoSmall channelView />
+                    <VideoSmall channelView />
+                    <VideoSmall channelView />
+                    <VideoSmall channelView />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default PreviewChannel;
--- src/components/VideoSmall/VideoSmall.jsx
+++ src/components/VideoSmall/VideoSmall.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./VideoSmall.scss";
 
-const VideoSmall = () => {
+const VideoSmall = ({ channelView = false }) => {
+    const navigate = useNavigate();
+    const handleClickRedirect = () => {
+        navigate("/watch");
+    };
+
     return (
-        <div className="videoSmall">
+        <div
+            onClick={handleClickRedirect}
+            className={`videoSmall ${channelView && "videoSmall__channelView"}`}
+        >
             <div className="videoSmall__left">
                 <img
                     src="https://i.ytimg.com/vi/_nSalXeU11Y/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD5g7epiuXklAkiM8OKPqIvDSL8Mg"
                     alt="thumbnail"
-                    className="videoSmall__thumbnail"
+                    className={`videoSmall__thumbnail ${
+                        channelView && "videoSmall__channelView__img"
+                    }`}
                 />
             </div>
 
@@ -16,7 +27,9 @@ const VideoSmall = () => {
                 <p className="videoSmall__title">Ronin songs</p>
 
                 <div className="videoSmall__texts videothumb__texts">
-                    <p className="videothumb__text">Amazon prime videp</p>
+                    {!channelView && (
+                        <p className="videothumb__text">Amazon prime videp</p>
+                    )}
 
                     <p className="videothumb__text">3,448 views • 5 days ago</p>
                 </div>
